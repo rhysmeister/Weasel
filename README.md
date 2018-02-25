1d00c70496636c619490d8680ca307e1037c0a8d
Rhys Campbell <rhyscampbell@rhyss-macbook-pro.home>
2018-02-25 15:09:56 +0100

    Added Twitter stuff

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Weasel
-A tool for downloading details of stuff posted you've posted Facebook
+A tool for downloading details of stuff posted you've posted Facebook & Twitter
+
+# Install Requirements
+
+sudo python -m pip install -e git+https://github.com/mobolic/facebook-sdk.git#egg=facebook-sdk
+sudo python -m pip install python-twitter
+
+# Facebook Auth
+
+Get an Access Token from teh Facebook Graph API Explorer https://developers.facebook.com/tools/explorer/
+
+# Twitter Auth
+
+Create the following dictionary in a file call twitter_auth.dict
+
+{
+  consumer_key='consumer_key',
+  consumer_secret='consumer_secret',
+  access_token_key='access_token',
+  access_token_secret='access_token_secret'
+}
+
+A detailed guide for this can be found here https://python-twitter.readthedocs.io/en/latest/
--- weasel.py
+++ weasel.py
@@ -2,7 +2,7 @@
 import argparse, facebook
 
 parser = argparse.ArgumentParser(description='Download your likes, posts and comments from Facebook')
-parser.add_argument('-t', '--access_token', required=True, type=str, help='Facebook Access Token. Get one from the Graphi API Explorer')
+parser.add_argument('-t', '--access_token', type=str, help='Facebook Access Token. Get one from the Graphi API Explorer')
 parser.add_argument('-v', '--version', type=str, default="2.11", help='The version of the Facebook Graph API')
 parser.add_argument('-l', '--limit', type=int, default=4000, help='The number of results to fetch per call to the Graph API')
 
@@ -50,11 +50,41 @@ def get_feed(access_token, version, limit):
 def get_photos(access_token, version, limit):
     get_generic_call(access_token, version, 'photos', limit)
 
-def call_all_gets(access_token, version, limit):
+def call_all_facebook(access_token, version, limit):
     get_likes(access_token, version, limit)
     get_posts(access_token, version, limit)
     get_friends(access_token, version, limit)
     get_feed(access_token, version, limit)
     get_photos(access_token, version, limit)
+
+def get_twitter_timeline(twitter_auth, username):
+    if 'consumer_key' in twitter_auth.keys():
+        import twitter
+        api = twitter.Api(consumer_key=twitter_auth['consumer_key'],
+                         consumer_secret=twitter_auth['consumer_secret'],
+                         access_token_key=twitter_auth['access_token_key'],
+                         access_token_secret=twitter_auth['access_token_secret'])
+        max_id = None
+        tweet_count = 0
+        call_count = 0
+        statuses = api.GetUserTimeline(screen_name=username, count=200)
+        while len(statuses) > 0:
+            call_count += 1
+            max_id_changed = False
+            for s in statuses:
+                if max_id is None:
+                    max_id = int(s.id)
+                elif int(s.id) < max_id:
+                        max_id = int(s.id)
+                print(max_id)
+                tweet_count += 1
+            statuses = api.GetUserTimeline(screen_name=username, max_id=max_id, count=200)
+        print("The number of calls performed was {0} and {1} tweets were retrieved.".format(call_count,
+                                                                                            tweet_count))
+
 # TODO? https://github.com/t-davidson/hate-speech-and-offensive-language/tree/master/lexicons
-call_all_gets(args.access_token, args.version, args.limit)
+
+if args.access_token is not None:
+    call_all_facebook(args.access_token, args.version, args.limit)
+
+twitter_auth = dict = eval(open("./twitter_auth.dict").read())
