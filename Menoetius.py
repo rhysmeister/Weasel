2cc3c5da06afd3b45585292ce91e25bb79c41dad
Rhys Campbell <rhys.james.campbell@googlemail.com>
2018-03-02 16:32:47 +0100

    First commit of class

---
+++ Menoetius.py
@@ -0,0 +1,21 @@
+"""
+Menoetius is the Greek God of Rudeness - http://www.godchecker.com/pantheon/greek-mythology.php?deity=MENOETIUS
+
+This Python class is an attempt to implement some of the ideas found in the following paper;
+
+"Automated Hate Speech Detection and the Problem of Offensive Language"
+
+https://arxiv.org/pdf/1703.04009.pdf
+
+Stemming - https://pypi.python.org/pypi/stemming/1.0
+Categorizing and Tagging Words - http://www.nltk.org/book/ch05.html
+Text stats - https://pypi.python.org/pypi/textstat
+
+ pip install stemming textstat nltk
+
+"""
+
+class Menoetius:
+
+    def __init__():
+        pass
